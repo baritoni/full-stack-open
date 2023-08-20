0d9652804901c3a47687098435f060e4a59fb51c
Toni Luomala <aa4498@student.jamk.fi>
2023-08-20 20:47:18 +0300

    valmis palautukseen

--- osa 4/blogilista/controllers/blogs.js
+++ osa 4/blogilista/controllers/blogs.js
@@ -1,74 +1,45 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-const { userExtractor, tokenExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('bearer ')) {
-    return authorization.replace('bearer ', '')
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+  console.log('request.body: ', request.body)
+
+  const user = request.user
+
+  const blog = new Blog({
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
+  if (!blog.title || !blog.url) {
+    return response.status(400).json({ error: 'url or title missing' })
   }
-  return null
-}
 
-blogsRouter.post(
-  '/',
-  userExtractor,
-  tokenExtractor,
-  async (request, response, next) => {
-    const body = request.body
-    /*
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  const user = request.user
+  if (blog.user.toString() === user.id.toString()) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: 'Not valid user for operation' })
   }
-  */
-    const user = request.user
-
-    const blog = new Blog({
-      author: body.author,
-      title: body.title,
-      url: body.url,
-      likes: body.likes,
-      user: user._id
-    })
-
-    if (!blog.title || !blog.url) {
-      return response.status(400).json({ error: 'url or title missing' })
-    }
-
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-  }
-)
-
-blogsRouter.delete(
-  '/:id',
-  userExtractor,
-  tokenExtractor,
-  async (request, response) => {
-    //const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-
-    const blog = await Blog.findById(request.params.id)
-    const user = request.user
-    if (blog.user.toString() === user.id.toString()) {
-      await Blog.findByIdAndRemove(request.params.id)
-      response.status(204).end()
-    } else {
-      return response
-        .status(401)
-        .json({ error: 'Not valid user for operation' })
-    }
-  }
-)
+})
 
 blogsRouter.put('/:id', async (request, response, next) => {
   const body = request.body
