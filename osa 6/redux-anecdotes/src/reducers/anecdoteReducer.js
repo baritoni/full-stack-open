d2162dc92ed55e7a9127c833afe799bbaaf6b304
Toni Luomala <aa4498@student.jamk.fi>
2023-09-13 15:49:18 +0300

    tehtävä 6.11 valmiiksi

--- osa 6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa 6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,50 +19,8 @@ export const asObject = (anecdote) => {
   }
 }
 
-/*
-export const voteSelected = (id) => {
-  return {
-    type: 'VOTE',
-    payload: { id }
-  }
-}
-
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: {
-      content,
-      id: getId(),
-      votes: 0
-    }
-  }
-}
-*/
 const initialState = anecdotesAtStart.map(asObject)
-/*
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  switch (action.type) {
-    case 'NEW_ANECDOTE':
-      return [...state, action.payload]
-    case 'VOTE': {
-      const id = action.payload.id
-      const anecdoteToVote = state.find((a) => a.id === id)
 
-      const changedAnecdote = {
-        ...anecdoteToVote,
-        votes: anecdoteToVote.votes + 1
-      }
-
-      return state.map((anecdote) =>
-        anecdote.id !== id ? anecdote : changedAnecdote
-      )
-    }
-    default:
-      return state
-  }
-}
-*/
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState,
